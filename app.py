270742caf983c0069b3b917a1740bf8b3d372a29
Akshay Prabhat Mishra <akshaypmishra02@gmail.com>
2022-10-01 01:49:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def index_post():
     target_language = request.form['language']
 
     # Load the values from .env
-    key = os.environ['KEY']
-    endpoint = os.environ['ENDPOINT']
-    location = os.environ['LOCATION']
+    key = env.['KEY']
+    endpoint = secrets.env.['ENDPOINT']
+    location = secrets.env.['LOCATION']
 
     # Indicate that we want to translate and the API version (3.0) and the target language
     path = '/translate?api-version=3.0'
